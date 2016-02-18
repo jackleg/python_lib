9423d07b9a49eb93a550f07d1bb404c440aa4baf
한승균 <jay.han@daumkakao.com>
2016-02-18 10:32:56 +0900

    주석 제거.

--- dfutil.py
+++ dfutil.py
@@ -16,7 +16,6 @@ def find_by_value(df, column_name, value, first=True):
     :param first: 동일한 값이 여러 개 있는 경우 처음 나타나는 값을 반환할 지 여부. False이면 마지막 값을 반환.
     :return: df[column_name] == value인 index. 해당하는 값이 없다면 None.
     """
-    df[column_name] == value인 df의 index를 반환한다.
     index_list = df[df[column_name] == value].index.tolist()
 
     if index_list:
