7b32348303ed54f56856653e62dc9eb98fc98e27
jay <jackleg83@gmail.com>
2016-08-19 20:58:52 +0900

    display_full 및 tsv, json 포맷 파일을 읽는 함수 추가.

--- putils/dfutil.py
+++ putils/dfutil.py
@@ -2,7 +2,8 @@
 
 """pandas.DataFrame 관련 utility"""
 
-
+import json
+import csv
 import pandas as pd
 import numpy as np
 
@@ -41,3 +42,42 @@ def has_null(ser, need_count=False):
         return pd.Series([(count > 0), count], index=["has_null", "count"])
     else:
         return any(rel_list)
+
+
+###############################################################################
+# display
+###############################################################################
+def display_full(df):
+    """
+    truncation view로 들어가지 않고, df를 모두 출력한다.
+    """
+    with pd.option_context("display.max_rows", len(df)), pd.option_context("display.max_columns", len(df.columns)):
+        display(df)
+
+
+###############################################################################
+# data_util
+###############################################################################
+def read_tsv(filepath_or_buffer, **kwargs):
+    """seperator가 tab인 csv 읽기
+    
+    아래와 같은 옵션이 default로 셋팅된 read_csv의 wrapper.
+    sep = tab
+    header = None
+    quoting = csv.QUOTE_NONE
+    """
+    sep = "\t" if "sep" not in kwargs else kwargs.pop("sep")
+    header = None if "header" not in kwargs else kwargs.pop("header")
+    quoting = csv.QUOTE_NONE if "quoting" not in kwargs else kwargs.pop("quoting")
+
+    return pd.read_csv(filepath_or_buffer, sep=sep, header=header, quoting=quoting, **kwargs)
+
+
+def read_json(filepath_or_buffer, index_col=None):
+    """row 데이터가 json 포맷으로 구성된 데이터 읽기"""
+
+    with open(filepath_or_buffer) as infile:
+        df = pd.DataFrame([pd.Series(json.loads(line)) for line in infile])
+
+        if index_col: return df.set_index(index_col)
+        else: return df
