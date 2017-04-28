c59a3ef015df9a887ed8f3a2729df8b76f36aebe
jay.han <jackleg83@gmail.com>
2017-04-28 17:40:50 +0900

    project 설명 추가.

--- putils/__init__.py
+++ putils/__init__.py
@@ -1,5 +1,13 @@
+"""
+    python utils
+    ~~~~~~~~~~~~
+"""
+
 import dfutil
 import statsutil
 import strutil
 import util
 
+__author__ = "jay.han"
+__email__ = "jackleg83@gmail.com"
+__version__ = "0.1"
