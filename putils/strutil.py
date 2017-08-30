4548f0b5fcd3a44ed0309fa8f8ab2b7d9ee69de6
Seungkyun Han <jay.han@sk.com>
2017-08-30 18:54:11 +0900

    한글 자모 분리 관련 함수들 추가.

--- putils/strutil.py
+++ putils/strutil.py
@@ -3,6 +3,14 @@
 """string 관련 처리 모듈."""
 
 import re
+import itertools
+
+
+# constants
+HANGUL_SYLLABLE_BASE=0xAC00 # == '가'
+HANGUL_CHOSEONG_BASE=0x1100
+HANGUL_JUNGSEONG_BASE=0x1161
+HANGUL_JONGSEONG_BASE=0x11A7
 
 
 def str_join(data_list, sep="\t"):
@@ -40,6 +48,82 @@ def has_hangul(haystack, encoding="utf8"):
         raise TypeError("haystack must be str, or unicode. (%s)[%s] received." % (haystack, type(haystack)))
 
     return (hangul.search(haystack) is not None)
+
+
+def is_hangul_syllable(needle, encoding="utf8"):
+    """주어진 글자가 한글인지 아닌지 판단.
+
+    :param needle: 검사할 글자
+    :param encoding: needle이 unicode가 아닌 경우 사용된 encoding.
+    :return: needle이 한글 자모 혹은 하나의 음절이면 True, 아니면 False.
+    """
+
+    if isinstance(needle, str):
+        needle = needle.decode(encoding)
+    elif isinstance(needle, unicode):
+        pass
+    else:
+        raise TypeError("needle must be str or unicode. (%s)[%s] received." % (needle, type(needle)))
+
+    return u'가' <= needle and needle <= u'힣'
+
+
+def expand_syllable_to_jamo(needle, encoding="utf8"):
+    """주어진 글자를 자모로 분리한다.
+
+    주어진 글자가 한글의 온전한 음절인 경우, 해당 글자를 자모로 분리한 리스트를 반환한다.
+    그렇지 않은 경우 자모 분리를 하지 않고 글자 그대로 리스트에 담아서 반환한다.
+
+    :param needle: 분석 대상 글자
+    :param encoding: needle이 unicode가 아닌 경우 사용된 encoding.
+    :return: needle이 자모로 분석된 글자들의 리스트. needle이 한글이 아닌 경우 글자가 그대로 들어가 있다.
+    """
+
+    if isinstance(needle, str):
+        needle = needle.decode(encoding)
+    elif isinstance(needle, unicode):
+        pass
+    else:
+        raise TypeError("needle must be str or unicode. (%s)[%s] received." % (needle, type(needle)))
+
+    if is_hangul_syllable(needle):
+        needle_index = ord(needle) - HANGUL_SYLLABLE_BASE
+        jongseong_index = needle_index % 28
+        jungseong_index = ((needle_index - jongseong_index) / 28) % 21
+        choseong_index = (((needle_index - jongseong_index) / 28) - jungseong_index) / 21
+
+        if jongseong_index == 0:
+            return [unichr(choseong_index + HANGUL_CHOSEONG_BASE), unichr(jungseong_index + HANGUL_JUNGSEONG_BASE)]
+        else:
+            return [unichr(choseong_index + HANGUL_CHOSEONG_BASE),
+                    unichr(jungseong_index + HANGUL_JUNGSEONG_BASE),
+                    unichr(jongseong_index + HANGUL_JONGSEONG_BASE)]
+    else:
+        return [needle]
+        
+
+def expand_string_to_jamo(haystack, encoding="utf8"):
+    """haystack에 한글이 포함된 경우에 자모로 분리한다.
+
+    haystack에 한글이 있는 경우에는 한글을 자모로 분리하고,
+    그 외의 글자들은 그대로 유지한다.
+
+    :param haystack: 분석 대상 string
+    :param encoding: haystack이 unicode가 아닌 경우 사용된 encoding.
+    :return: haystack의 한글이 자모로 분석된 글자들의 리스트. 한글이 아닌 글자는 변형되지 않는다.
+    """
+
+    if isinstance(haystack, str):
+        haystack = haystack.decode(encoding)
+    elif isinstance(haystack, unicode):
+        pass
+    else:
+        raise TypeError("haystack must be str or unicode. (%s)[%s] received." % (haystack, type(haystack)))
+
+    expanded_list = map(expand_syllable_to_jamo, haystack)
+    merged_list = list(itertools.chain.from_iterable(expanded_list))
+
+    return merged_list
 
 
 def convert_to_str(may_unicode, encoding="utf8"):
